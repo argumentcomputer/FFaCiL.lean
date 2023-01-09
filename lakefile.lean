f1e385559e787bb18e18f3fd348fc492ba52a765
DanielRrr <relationalgebra@gmail.com>
2023-01-09 15:35:24 +0000

    add GaloisField.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,6 +1,10 @@
 import Lake
 open Lake DSL
 
-package FF.lean {
-  -- add configuration options here
-}
+package FF
+
+@[default_target]
+lean_lib FF
+
+require YatimaStdLib from git
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "515b7eff7765dcf55ce275ac41fa13a30a59f1d0"
