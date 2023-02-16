5ea6858919166c4093ef829b69aed7ec310e50b2
mpenciak <matej.penciak@gmail.com>
2023-02-16 16:15:54 -0500

    sketch 2?

--- FF/EllipticCurves2.lean
+++ FF/EllipticCurves2.lean
@@ -1,8 +1,7 @@
 import FF.NewField
 
-variable (F : Type _) [PrimeField F]
-
-class Curve (_ : Type _) (F : Type _) [PrimeField F] where
+/-- Curves with Weierstrass form-/
+class Curve (K : Type _) (F : outParam (Type _)) [PrimeField F] where
   a : F
   b : F
   order : Nat
@@ -20,19 +19,24 @@ def jacobianForm (C : Type _) [PrimeField F] [Curve C F] : F × F := sorry
 
 end Curve
 
-class CurvePoint (C : Type _) (K : Type _) [PrimeField F] [Curve C F] where
+class CurvePoint {F : Type _} (C : Type _) (K : Type _) [PrimeField F] [Curve C F] where
+  zero : K
+  inv : K → K 
+  add : K → K → K
+  double : K → K := fun x => add x x
+  smul : Nat → K → K  
 
-
-structure AffinePoint (F) where
+structure AffinePoint (F : Type _) [PrimeField F] where
   x : F
   y : F
+  isInfty : Bool
 
-structure ProjectivePoint (F) where
+structure ProjectivePoint (F : Type _) [PrimeField F] where
   x : F
   y : F
   z : F
 
-def ProjectivePoint.isInfinity : ProjectivePoint F → Bool
+def ProjectivePoint.isInfinity {F : Type _} [PrimeField F] : ProjectivePoint F → Bool
   | c => c.z == 0
 
 -- P₁ : AffinePoint, P₂ : ProjectivePoint ==> P₁ + P₂
@@ -40,14 +44,27 @@ def ProjectivePoint.isInfinity : ProjectivePoint F → Bool
 -- (x, y, z) : ProjectivePoint => (x/z, y/z) : AffinePoint
 -- If z ≠ 0
 -- If z = 0
---  
 
--- instance : CurvePoint C (AffinePoint F) where
---   sorry
+instance {F} [PrimeField F] [Curve C F] : CurvePoint C (AffinePoint F) where
+  zero := sorry
+  inv := sorry
+  add := fun ⟨x, y, i⟩ ⟨u, v, j⟩ => 
+    match i, j with
+    | true, true => sorry
+    | true, false => sorry
+    | false, true => sorry
+    | false, false => sorry
+  smul := sorry
+
+instance {F} [PrimeField F] [Curve C F] : CurvePoint C (ProjectivePoint F) where
+  zero := sorry
+  inv := sorry
+  add := sorry
+  smul := sorry
 
 structure BLS12381 where
 
-instance [NewField F] [OfNat F 4] : Curve BLS12381 F where
+instance [PrimeField F] [OfNat F 4] : Curve BLS12381 F where
   a := 0
   b := 4
   order := 0x923480928340981
