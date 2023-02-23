a5576a71ec425d6f1bae257fdb1580849ba0c91f
DanielRrr <relationalgebra@gmail.com>
2023-02-23 17:11:45 +0000

    thundering dissapointment

--- Test/EllipticCurve.lean
+++ Test/EllipticCurve.lean
@@ -25,3 +25,32 @@ instance [PrimeField F] : Curve BLS12381 F where
   characteristic := sorry
 
 end bigtest
+
+section notsobigtest
+
+new_field Test101 with
+  prime: 101
+  generator: 1   
+
+structure Kitten where
+
+instance cur : Curve Kitten Test101 where
+  a := 2
+  b := 3
+  order := 96
+  cofactor := sorry
+  characteristic := sorry
+
+instance : CurvePoint Kitten (AffinePoint Test101) where
+  zero := aff.zero
+  inv := aff.inv
+  add := aff.add
+  toPoint := aff.toPoint
+  frobenius := aff.frobenius
+
+def p₁ : AffinePoint Test101 := ⟨ 23, 57, false ⟩
+def p₂ : AffinePoint Test101 := ⟨ 31, 74, false ⟩
+
+#eval @smul Test101 Kitten (AffinePoint Test101) _ _ _ (5 : Nat) p₁
+
+end notsobigtest
