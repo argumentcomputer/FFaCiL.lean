dea9dd6a89edaa8175986cbf686d22ce179148f7
mpenciak <matej.penciak@gmail.com>
2023-02-28 23:31:48 -0500

    some method and instance changes

--- FF/EllipticCurve.lean
+++ FF/EllipticCurve.lean
@@ -7,11 +7,12 @@ TODO: Major items to consider before we can finally settle on this design:
 * 
 
 -/
+
 /--
 Curves with Weierstrass form satisfying the equation `y² = x³ + a x + b`
 for a prime field `F` such that `char K > 3`
 -/
-structure Curve (F : Type _) [PrimeField F] where
+structure Curve (F : Type _) [Field F] where
   a : F
   b : F
 
@@ -31,22 +32,22 @@ TODO: Add more curve point operations
 * Frobenius? (Only makes sense if we define curves over `Galois Fields`)
 -/
 
-structure ProjectivePoint {F : Type _} [PrimeField F] (C : Curve F) where
+structure ProjectivePoint {F : Type _} [Field F] (C : Curve F) where
   X : F
   Y : F
   Z : F
   deriving BEq
 
-instance [ToString F] [PrimeField F] {C : Curve F} : ToString $ ProjectivePoint C where
+instance [ToString F] [Field F] {C : Curve F} : ToString $ ProjectivePoint C where
   toString := fun ⟨x, y, z⟩ => s!"({x} : {y} : {z})"
 
-def ProjectivePoint.isInfinity {F : Type _} [PrimeField F] {C : Curve F} (P : ProjectivePoint C) :=
+def ProjectivePoint.isInfinity {F : Type _} [Field F] {C : Curve F} (P : ProjectivePoint C) :=
   P.X == 0 && P.Y == 1 && P.Z == 0
 
-def ProjectivePoint.infinity {F : Type _} [PrimeField F] {C : Curve F} : ProjectivePoint C :=
+def ProjectivePoint.infinity {F : Type _} [Field F] {C : Curve F} : ProjectivePoint C :=
   ⟨0, 1, 0⟩
 
-def ProjectivePoint.double [PrimeField F] {C : Curve F} : ProjectivePoint C → ProjectivePoint C
+def ProjectivePoint.double [Field F] {C : Curve F} : ProjectivePoint C → ProjectivePoint C
   | ⟨x, y, z⟩ =>
     let a := C.a * z^2 + (3 : Nat) * x^2
     let b := y * z
@@ -57,47 +58,50 @@ def ProjectivePoint.double [PrimeField F] {C : Curve F} : ProjectivePoint C → 
     let z₁ := (8 : Nat) * b^3
     ⟨x₁, y₁, z₁⟩
 
-def ProjectivePoint.add {F : Type _} [PrimeField F] {C : Curve F} (p₁ p₂ : ProjectivePoint C) : ProjectivePoint C :=
-  if p₁ == p₂ then ProjectivePoint.double p₁ else
-  match p₁, p₂ with
-  | ⟨x₁, y₁, z₁⟩, ⟨x₂, y₂, z₂⟩ => 
-    let a := y₂ * z₁ - y₁ * z₂
-    let b := x₂ * z₁ - x₁ * z₂
-    let c := a^2 * z₁ * z₂ - b^3 - (2 : Nat) * b^2 * x₁ * z₂
-    let x₃ := b * c
-    let y₃ := a * (b^2 * x₁ * z₂ - c) - b^3 * y₁ * z₂
-    let z₃ := b^3 * z₁ * z₂
-    ⟨x₃, y₃, z₃⟩
+def ProjectivePoint.add {F : Type _} [Field F] {C : Curve F} (p₁ p₂ : ProjectivePoint C) 
+  : ProjectivePoint C :=
+    if p₁ == p₂ then ProjectivePoint.double p₁ else
+    match p₁, p₂ with
+    | ⟨x₁, y₁, z₁⟩, ⟨x₂, y₂, z₂⟩ => 
+      let a := y₂ * z₁ - y₁ * z₂
+      let b := x₂ * z₁ - x₁ * z₂
+      let c := a^2 * z₁ * z₂ - b^3 - (2 : Nat) * b^2 * x₁ * z₂
+      let x₃ := b * c
+      let y₃ := a * (b^2 * x₁ * z₂ - c) - b^3 * y₁ * z₂
+      let z₃ := b^3 * z₁ * z₂
+      ⟨x₃, y₃, z₃⟩
 
-inductive AffinePoint {F : Type _} [PrimeField F] (C : Curve F) where
+inductive AffinePoint {F : Type _} [Field F] (C : Curve F) where
   | affine (X : F) (Y : F) : AffinePoint C
   | infinity : AffinePoint C
 
-def AffinePoint.add {F : Type _} [PrimeField F] {C : Curve F} : AffinePoint C → AffinePoint C → AffinePoint C
-  | .infinity, _ => .infinity
-  | _, .infinity => .infinity
-  | .affine x₁ y₁, .affine x₂ y₂ =>
-      let lambda := (y₁ + y₂) / (x₁ + y₂)
-      let x₃ := lambda^2 + lambda + x₁ + x₂ + Curve.a C
-      let y₃ := lambda * (x₁ + x₃) + x₃ + x₁
-      .affine x₃ y₃
+def AffinePoint.add {F : Type _} [Field F] {C : Curve F} 
+  : AffinePoint C → AffinePoint C → AffinePoint C
+    | .infinity, _ => .infinity
+    | _, .infinity => .infinity
+    | .affine x₁ y₁, .affine x₂ y₂ =>
+        let lambda := (y₁ + y₂) / (x₁ + y₂)
+        let x₃ := lambda^2 + lambda + x₁ + x₂ + Curve.a C
+        let y₃ := lambda * (x₁ + x₃) + x₃ + x₁
+        .affine x₃ y₃
 
-class CurveGroup {F : Type _} [PrimeField F] (C : Curve F) (K : outParam $ Type _) where 
+class CurveGroup {F : Type _} [Field F] (C : Curve F) (K : outParam $ Type _) where 
   zero : K
   inv : K → K
   add : K → K → K
   double : K → K
-  toPoint : F → F → Option K
-  frobenius : K → K
+  -- toPoint : F → F → Option K -- TODO: I think we should add this to `ProjectivePoint` and
+                                -- `AffinePoint` separately
+  -- frobenius : K → K -- TODO: I'm not sure we need/want Frobenius for `CurveGroup`
 
-instance {F K : Type _} [PrimeField F] (C : Curve F) [CurveGroup C K] : Add K where
+instance {F K : Type _} [Field F] (C : Curve F) [CurveGroup C K] : Add K where
   add := CurveGroup.add C
 
-instance {F K : Type _} [PrimeField F] (C : Curve F) [CurveGroup C K] : Neg K where
+instance {F K : Type _} [Field F] (C : Curve F) [CurveGroup C K] : Neg K where
   neg := CurveGroup.inv C
 
 open CurveGroup in
-partial def smulAux [PrimeField F] (C : Curve F)
+partial def smulAux [Field F] (C : Curve F)
   [CurveGroup C K] (n : Nat) (p : K) (acc : K) : K :=
   if n == 0 then acc
   else match n % 2 == 0 with
@@ -108,29 +112,29 @@ open CurveGroup in
 /--
 Montgomery's ladder for fast scalar-point multiplication
 -/
-def smul [PrimeField F] {C : Curve F}
+def smul [Field F] {C : Curve F}
   [CurveGroup C K] (n : Nat) (p : K) : K := smulAux C n p (zero C)
 
-instance {F K : Type _} [f : PrimeField F] (C : Curve F) [gr : CurveGroup C K] : HMul Nat K K where
+instance {F K : Type _} [f : Field F] (C : Curve F) [gr : CurveGroup C K] : HMul Nat K K where
   hMul := @smul F K f C gr
   
 open ProjectivePoint in
-instance {F : Type _} [PrimeField F] {C : Curve F} : CurveGroup C (ProjectivePoint C) where 
+instance {F : Type _} [Field F] {C : Curve F} : CurveGroup C (ProjectivePoint C) where 
   zero := infinity
   inv := fun ⟨x, y, z⟩ => ⟨x, 0 - y, z⟩ 
   add := ProjectivePoint.add
   double := ProjectivePoint.double
-  toPoint x y :=
-    let p := ⟨x, y, 1⟩
-    let isDef := fun (⟨x, y, z⟩ : ProjectivePoint C) =>
-      (x * x + C.a * z * z) * x == (y * y - C.b * z * z) * z
-    if isDef p then some p else none
-  frobenius :=
-    fun ⟨x, y, z⟩ =>
-    let frob := fun (x : F) => x^(PrimeField.char F)
-    ⟨ frob x, frob y, frob z⟩
+  -- toPoint x y :=
+  --   let p := ⟨x, y, 1⟩
+  --   let isDef := fun (⟨x, y, z⟩ : ProjectivePoint C) =>
+  --     (x * x + C.a * z * z) * x == (y * y - C.b * z * z) * z
+  --   if isDef p then some p else none
+  -- frobenius :=
+  --   fun ⟨x, y, z⟩ =>
+  --   let frob := fun (x : F) => x^(Field.char F)
+  --   ⟨ frob x, frob y, frob z⟩
 
-def affineDouble [PrimeField F] {C : Curve F} :
+def affineDouble [Field F] {C : Curve F} :
   AffinePoint C → AffinePoint C
   | .affine x y =>
     let lambda := ((3 : Nat) * x^2 + Curve.a C) / (2 : Nat) * y
@@ -139,7 +143,7 @@ def affineDouble [PrimeField F] {C : Curve F} :
     .affine x' y'
   | .infinity => .infinity
 
-instance {F : Type _} [PrimeField F] {C : Curve F} : CurveGroup C (AffinePoint C) where 
+instance {F : Type _} [Field F] {C : Curve F} : CurveGroup C (AffinePoint C) where 
   zero := .infinity
   inv p :=
     match p with
@@ -147,11 +151,11 @@ instance {F : Type _} [PrimeField F] {C : Curve F} : CurveGroup C (AffinePoint C
       | x           => x
   add := AffinePoint.add
   double := affineDouble
-  toPoint x y :=
-    let p := .affine x y
-    if (x * x + C.a * x) * x + C.b == y * y then some p else none
-  frobenius p :=
-    match p with
-      | .infinity => .infinity
-      | .affine x y => .affine (x^(PrimeField.char F)) (y^(PrimeField.char F))
+  -- toPoint x y :=
+  --   let p := .affine x y
+  --   if (x * x + C.a * x) * x + C.b == y * y then some p else none
+  -- frobenius p :=
+  --   match p with
+  --     | .infinity => .infinity
+  --     | .affine x y => .affine (x^(Field.char F)) (y^(Field.char F))
 
