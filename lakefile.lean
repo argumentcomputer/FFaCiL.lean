e3b320d44e7d1287738fa9282a6a4252f5c8510f
DanielRrr <relationalgebra@gmail.com>
2023-03-16 21:33:45 +0000

    add LightData as a dependency

--- lakefile.lean
+++ lakefile.lean
@@ -6,6 +6,9 @@ package FFaCiL
 @[default_target]
 lean_lib FFaCiL
 
+require LightData from git
+  "https://github.com/yatima-inc/LightData" @ "7492abeae41f3a469396c73e13f48cd2b8da4a02" 
+
 require YatimaStdLib from git
   "https://github.com/yatima-inc/YatimaStdLib.lean" @ "78b02f02367cdf3bfa1b65c0efbd40e56a47588b"
 
