0a5d697430f50417c9bee1dcf5d90c13de84cfbf
Matej Penciak <matej.penciak@gmail.com>
2023-03-15 23:35:32 -0400

    update ysl and std dependency

--- lakefile.lean
+++ lakefile.lean
@@ -7,9 +7,12 @@ package FF
 lean_lib FF
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "f616da79a3ffe29d1a04b956ed36ebfdc6e82a7f"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "e1e1c681e0a97176ae9e73ac5b0257fdd9b07ca4"
 
 require LSpec from git
   "https://github.com/yatima-inc/lspec/" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
+  
+require std from git
+  "https://github.com/leanprover/std4/" @ "fde95b16907bf38ea3f310af406868fc6bcf48d1"
 
 lean_exe Tests.EllipticCurve
