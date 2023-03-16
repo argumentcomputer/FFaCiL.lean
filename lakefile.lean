0b818531ba9a625d062ab787f06f29fbc70f1aae
DanielRrr <relationalgebra@gmail.com>
2023-03-16 13:52:33 +0000

    add sqrt to PrimeField

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ package FFaCiL
 lean_lib FFaCiL
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "f616da79a3ffe29d1a04b956ed36ebfdc6e82a7f"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "71e21a147378c91c8294911b2597ac9c26fae2ad"
 
 require LSpec from git
   "https://github.com/yatima-inc/lspec/" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
