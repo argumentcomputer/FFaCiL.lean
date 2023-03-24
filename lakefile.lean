9a4c77f8b703b60ae4e353b957e858a2e1de2e83
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-03-24 11:37:23 -0300

    update deps org (#17)

--- lakefile.lean
+++ lakefile.lean
@@ -7,12 +7,12 @@ package FFaCiL
 lean_lib FFaCiL
 
 require LightData from git
-  "https://github.com/yatima-inc/LightData" @ "7492abeae41f3a469396c73e13f48cd2b8da4a02" 
+  "https://github.com/lurk-lab/LightData" @ "7492abeae41f3a469396c73e13f48cd2b8da4a02" 
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "df30d88d06e22d7c418fb8eee2996dd44cd1c547"
+  "https://github.com/lurk-lab/YatimaStdLib.lean" @ "df30d88d06e22d7c418fb8eee2996dd44cd1c547"
 
 require LSpec from git
-  "https://github.com/yatima-inc/lspec/" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
+  "https://github.com/lurk-lab/LSpec" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
 
 lean_exe Tests.EllipticCurve
